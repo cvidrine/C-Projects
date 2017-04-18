dbd5c2466c2337332bd6e81114dbdea1649740b4
cvidrine <cvidrine@stanford.edu>
2017-04-17 20:30:00 -0700

    Create ArrayPriorityQueue.h

---
+++ PriorityQueues/ArrayPriorityQueue.h
@@ -0,0 +1,31 @@
+#ifndef _arraypriorityqueue_h
+#define _arraypriorityqueue_h
+ 
+#include <iostream>
+#include <string>
+#include "PQEntry.h"
+using namespace std;
+ 
+// TODO: comment
+class ArrayPriorityQueue {
+public:
+    ArrayPriorityQueue();
+    ~ArrayPriorityQueue();
+    void changePriority(string value, int newPriority);
+    void clear();
+    string dequeue();
+    void enqueue(string value, int priority);
+    bool isEmpty() const;
+    string peek() const;
+    int peekPriority() const;
+    int size() const;
+    friend ostream& operator <<(ostream& out, const ArrayPriorityQueue& queue);
+private:
+    PQEntry* data;
+    int usedSize;
+    int allocatedSize;
+    void doubleAllocation();
+ 
+};
+ 
+#endif
